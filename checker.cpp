2176d812bc963e8b3088ac354464093a252b29a9
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-04 18:08:20 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -25,25 +25,7 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
  return batteryHealthIsGood;
 }
       
-                                                            
-                                                        
-                                                        
-                                                    
-                                                        
-  /*if(!(everythingIsInRange(temperature, 0, 45))) {
-    cout << "Temperature out of range!\n";
-    batteryHealthIsGood = false;
-     } else if(!(everythingIsInRange(soc, 20 , 80))){
-    cout << "State of Charge out of range!\n";
-    batteryHealthIsGood = false;
-  } else if(!(everythingIsInRange(chargeRate, 0.0 , 0.8))) {
-    batteryHealthIsGood = false;
-    cout << "Charge Rate out of range!\n";
-  }
-  return batteryHealthIsGood;
-}*/
-
-int main() {
+ int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
 }
