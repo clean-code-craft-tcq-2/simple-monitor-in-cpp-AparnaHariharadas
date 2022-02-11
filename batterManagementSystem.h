6d94e2302a39251c45d7c44c912eb7eac4a78c74
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-11 19:55:08 +0530

    Update batterManagementSystem.h

--- batterManagementSystem.h
+++ batterManagementSystem.h
@@ -9,20 +9,22 @@ using namespace std;
 #define CHARGERATE_UPPER_LIMIT 0.8
 #define CHARGERATE_LOWER_LIMIT 0.0
 #define NO_OF_PARAMETERS 3
+float lowerLimit[3] = {0.0,20.0, 0.0};
+float upperLimit[3] = {45.0,80.0, 0.8};
+string parameterNames = {"Temperature", "SOC", "ChargeRate"};
 class batteryManagementClass
 {
-struct batteryManagementRange
-{
- float lLimit;
- float hLimit;
- float actualValue;
- char paramName;
-}
- 
- bool checkBatteryHealthParametersInRange() = 0;
-}
 
-batteryManagementStucture isParametersInRange[NO_OF_PARAMETERS]={0};
-//batteryManagementStucture checkSOCRange[NO_OF_PARAMETERS]={0};
-//batteryManagementStucture checkChangeRateRange[NO_OF_PARAMETERS]={0};
+ struct batteryManagementRange
+ {
+  float lLimit;
+  float hLimit;
+  float actualValue;
+ }
+ struct parameters
+ {
+  struct batteryManagementRange paramName;
+  }
+  bool checkBatteryHealthParametersInRange() = 0;
+ }
 
