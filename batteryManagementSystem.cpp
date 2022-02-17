910f937d4e8d3de715e18ea8e4387dcf3fd74bd3
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-17 11:21:29 +0530

    Update batteryManagementSystem.cpp

--- batteryManagementSystem.cpp
+++ batteryManagementSystem.cpp
@@ -8,12 +8,12 @@ void printParameterStatusOnConsole(string parameterName, string message){
     cout << parameterName << message;
 }
 
-void CheckParametersForEarlyWarning(batteryManagementClass::batteryManagementRange parameter)
+string CheckParametersForEarlyWarningAndMessage(batteryManagementClass::batteryManagementRange parameter)
 {
     if ((parameter.actualValue - parameter.lowerWaringLimitmax) * (parameter.actualValue - parameter.lLimit) <= 0)
-         printParameterStatusOnConsole(parameter.paramName, " Lower limit WARNING!\n");
+         return " Lower limit WARNING!\n";
     else if ((parameter.actualValue - parameter.hLimit) * (parameter.actualValue - parameter.upperWaringLimitmin) <= 0)
-        printParameterStatusOnConsole(parameter.paramName, " Upper limit WARNING!\n");
+        return " Upper limit WARNING!\n";
 }
 
 bool checkBatteryHealthParametersInRange(batteryManagementClass::batteryManagementRange parameter){
@@ -46,6 +46,7 @@ bool batteryIsOk(string temp, float soc, float chargeRate) {
     float tempInCelsius;
     tempInCelsius = separateParameterValueAndUnit(temp);
     float paramActuals[NO_OF_PARAMETERS] ={tempInCelsius, soc, chargeRate};
+    string message;
     for (int i = 0; i < NO_OF_PARAMETERS; i++)
     {  
         //separateParameterValueAndUnit(paramActuals[i])/*for future use for other parameters
@@ -60,7 +61,8 @@ bool batteryIsOk(string temp, float soc, float chargeRate) {
             printParameterStatusOnConsole(parameter.paramName, " out of range!\n");
             return false;
         }
-    
+        else{
+            printParameterStatusOnConsole(parameter.paramName, CheckParametersForEarlyWarningAndMessage(parameter));    
     }
 
  return true;
